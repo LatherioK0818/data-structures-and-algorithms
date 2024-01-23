9323e7914e88b25b9f24c836a763750aec640e1a
LatherioK0818 <80198308+LatherioK0818@users.noreply.github.com>
2024-01-23 01:33:02 -0500

    coding challenge 10

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,37 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+class Node:
+    """
+    Node Class:
+    Represents a node in a linked list.
+
+    Attributes:
+    - value: The value stored in the node.
+    - next: A pointer to the next node in the linked list.
+    """
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
 class Stack:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.is_empty():
+            raise Exception("Stack is empty")
+        value = self.top.value
+        self.top = self.top.next
+        return value
+
+    def peek(self):
+        if self.is_empty():
+            raise Exception("Stack is empty")
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
