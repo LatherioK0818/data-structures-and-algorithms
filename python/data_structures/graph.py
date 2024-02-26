d34350185628bb0096ab783c50911e912ea2bafd
LatherioK0818 <80198308+LatherioK0818@users.noreply.github.com>
2024-02-26 10:58:21 -0500

    Code Challenge

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -1,16 +1,78 @@
+class Vertex:
+    """
+    A class to represent a vertex in a graph.
+    """
+    def __init__(self, value):
+        self.value = value
+
+class Edge:
+    """
+    A class to represent an edge in a graph.
+    It stores the target vertex and the weight of the edge.
+    """
+    def __init__(self, vertex, weight):
+        self.vertex = vertex
+        self.weight = weight
+
 class Graph:
     """
-    Put docstring here
+    A Graph class to represent a graph using an adjacency list.
     """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.adj_list = {}  # Key: Vertex object, Value: List of Edge objects
 
-    def some_method(self):
-        # method body here
-        pass
+    def add_node(self, value):
+        """
+        Add a vertex to the graph.
+        :param value: The value of the node.
+        :return: The Vertex object for the node.
+        """
+        if value not in self.adj_list:
+            vertex = Vertex(value)
+            self.adj_list[vertex] = []
+            return vertex
 
+    def add_edge(self, start, end, weight=1, bidirectional=True):
+        """
+        Add an edge between two vertices.
+        :param start: The start vertex object.
+        :param end: The end vertex object.
+        :param weight: The weight of the edge.
+        :param bidirectional: If True, adds an edge back from end to start as well.
+        """
+        if start not in self.adj_list:
+            self.add_node(start.value)
+        if end not in self.adj_list:
+            self.add_node(end.value)
 
-class Vertex:
-    pass
+        self.adj_list[start].append(Edge(end, weight))
+        if bidirectional:
+            self.adj_list[end].append(Edge(start, weight))
+
+    def get_nodes(self):
+        """
+        Return all nodes in the graph.
+        """
+        return [vertex.value for vertex in self.adj_list]
+
+    def get_neighbors(self, vertex):
+        """
+        Return all neighbors of a given vertex along with the edge weights.
+        :param vertex: The vertex object.
+        :return: A list of tuples (neighbor_vertex_value, weight).
+        """
+        return [(edge.vertex.value, edge.weight) for edge in self.adj_list[vertex]]
+
+    def size(self):
+        """
+        Return the number of vertices in the graph.
+        """
+        return len(self.adj_list)
+
+    def display(self):
+        """
+        Display the graph's adjacency list with edge weights.
+        """
+        for vertex, edges in self.adj_list.items():
+            edge_descriptions = ', '.join([f"({edge.vertex.value}, {edge.weight})" for edge in edges])
+            print(f"{vertex.value}: [{edge_descriptions}]")
--- python/tests/data_structures/test_graph.py
+++ python/tests/data_structures/test_graph.py
@@ -1,133 +1,63 @@
 import pytest
 from data_structures.graph import Graph, Vertex
 
-
-def test_exists():
-    assert Graph
-
-
-@pytest.mark.skip("TODO")
-def test_add_node():
-
+def test_vertex_addition():
     graph = Graph()
+    vertex = graph.add_node("A")
+    assert isinstance(vertex, Vertex)
+    assert vertex.value == "A"
 
-    expected = "spam"  # a vertex's value that comes back
-
-    actual = graph.add_node("spam").value
-
-    assert actual == expected
-
-
-@pytest.mark.skip("TODO")
-def test_size_empty():
-
+def test_edge_addition():
     graph = Graph()
+    start = graph.add_node("A")
+    end = graph.add_node("B")
+    graph.add_edge(start, end, 5)
+    assert len(graph.get_neighbors(start)) == 1
+    assert graph.get_neighbors(start)[0].vertex == end
+    assert graph.get_neighbors(start)[0].weight == 5
 
-    expected = 0
-
-    actual = graph.size()
-
-    assert actual == expected
-
-
-@pytest.mark.skip("TODO")
-def test_size():
-
+def test_retrieving_all_vertices():
     graph = Graph()
+    graph.add_node("A")
+    graph.add_node("B")
+    vertices = graph.get_nodes()
+    assert len(vertices) == 2
+    assert set(vertex.value for vertex in vertices) == {"A", "B"}
 
-    graph.add_node("spam")
-
-    expected = 1
-
-    actual = graph.size()
-
-    assert actual == expected
-
-
-@pytest.mark.skip("TODO")
-def test_add_edge():
-    g = Graph()
-    apple = g.add_node("apple")
-    banana = g.add_node("banana")
-    g.add_edge(apple, banana, 5)
-    neighbors = g.get_neighbors(apple)
+def test_retrieving_neighbors():
+    graph = Graph()
+    start = graph.add_node("A")
+    end = graph.add_node("B")
+    graph.add_edge(start, end, 5)
+    neighbors = graph.get_neighbors(start)
     assert len(neighbors) == 1
-    assert neighbors[0].vertex.value == "banana"
+    assert neighbors[0].vertex == end
     assert neighbors[0].weight == 5
 
-
-@pytest.mark.skip("TODO")
-def test_bouquet():
-    g = Graph()
-    apple = g.add_node("apple")
-    g.add_edge(apple, apple, 10)
-    neighbors = g.get_neighbors(apple)
-    assert len(neighbors) == 1
-    assert neighbors[0].vertex.value == "apple"
+def test_neighbors_with_weights():
+    graph = Graph()
+    start = graph.add_node("A")
+    end = graph.add_node("B")
+    graph.add_edge(start, end, 10)
+    neighbors = graph.get_neighbors(start)
     assert neighbors[0].weight == 10
 
-
-@pytest.mark.skip("TODO")
-def test_add_edge_interloper_start():
-
+def test_graph_size():
     graph = Graph()
+    graph.add_node("A")
+    graph.add_node("B")
+    assert len(graph.get_nodes()) == 2
 
-    start = Vertex("start")
-
-    end = graph.add_node("end")
-
-    with pytest.raises(KeyError):
-        graph.add_edge(start, end)
-
-
-@pytest.mark.skip("TODO")
-def test_add_edge_interloper_end():
-
+def test_single_vertex_edge_graph():
     graph = Graph()
-
-    end = Vertex("end")
-
-    start = graph.add_node("start")
-
-    with pytest.raises(KeyError):
-        graph.add_edge(start, end)
-
-
-@pytest.mark.skip("TODO")
-def test_get_nodes():
-
-    graph = Graph()
-
-    banana = graph.add_node("banana")
-
-    apple = graph.add_node("apple")
-
-    loner = Vertex("loner")
-
-    expected = 2
-
-    actual = len(graph.get_nodes())
-
-    assert actual == expected
-
-
-@pytest.mark.skip("TODO")
-def test_get_neighbors():
-
-    graph = Graph()
-
-    banana = graph.add_node("banana")
-
-    apple = graph.add_node("apple")
-
-    graph.add_edge(apple, banana, 44)
-
-    neighbors = graph.get_neighbors(apple)
-
+    loop_vertex = graph.add_node("A")
+    graph.add_edge(loop_vertex, loop_vertex, 1)
+    assert len(graph.get_nodes()) == 1
+    neighbors = graph.get_neighbors(loop_vertex)
     assert len(neighbors) == 1
+    assert neighbors[0].vertex == loop_vertex
+    assert neighbors[0].weight == 1
 
-    neighbor_edge = neighbors[0]
-
-    assert neighbor_edge.vertex.value == "banana"
-
-    assert neighbor_edge.weight == 44
+# Optional: Run tests if this script is executed directly
+if __name__ == "__main__":
+    pytest.main()
